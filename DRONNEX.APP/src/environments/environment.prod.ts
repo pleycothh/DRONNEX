d9c4185bf13fbe16c2ea85bcda0f07dfd7e9dff5
Zhuoheng Li <1050429760@qq.com>
2025-05-11 23:47:08 +1000

    link api

--- DRONNEX.APP/src/environments/environment.prod.ts
+++ DRONNEX.APP/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    baseUrl: 'https://simplefpv-api-bmb3h5d4cbb4b0h0.australiasoutheast-01.azurewebsites.net'
+    baseUrl: 'https://dronnex-api-a4dxfkc3bjhzdyaf.australiaeast-01.azurewebsites.net'
 };
