1742ab124c942df8df32d69db7045b470965846f
Zhuoheng Li <1050429760@qq.com>
2025-05-24 00:06:02 +1000

    add docusours

--- DRONNEX.APP/src/app/nav-top-group/nav-top-group.component.ts
+++ DRONNEX.APP/src/app/nav-top-group/nav-top-group.component.ts
@@ -2,17 +2,21 @@ import { Component, inject, signal } from '@angular/core';
 import { DarkModeService } from '../sharing/services/dark-mode.service';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-nav-top-group',
   standalone: true,
-  imports: [ CommonModule],
+  imports: [ 
+    CommonModule
+  ],
   templateUrl: './nav-top-group.component.html',
   styleUrl: './nav-top-group.component.scss'
 })
 export class NavTopGroupComponent {
   darkModeService: DarkModeService = inject(DarkModeService);
   selectedIcon: string = 'moon'; // Default selected icon
+  docsUrl = environment.docsUrl;
 
 constructor() { }
 //darkModeService.darkModeService
--- DRONNEX.APP/src/environments/environment.ts
+++ DRONNEX.APP/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    baseUrl: 'https://localhost:7173'
+    baseUrl: 'https://localhost:7173',
+    docsUrl: 'http://localhost:3000/doc'
 };
